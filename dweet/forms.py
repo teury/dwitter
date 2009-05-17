96d11f947d974b21e3a27cdd11c6f62d804c37fa
mozcelebi <mozcelebi@9b4810c6-2dba-11de-8e0c-53a27eea117e>
2009-05-17 19:55:41 +0000

    jquery modal sample

---
+++ dweet/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from dweet.models import Dweety
+
+class DweetForm(forms.ModelForm):
+    id = forms.IntegerField(widget=forms.HiddenInput,required=False)
+    class Meta:
+        model = Dweety
+        fields = ('id','value')
--- views.py
+++ views.py
@@ -1,11 +1,29 @@
-from django.http import HttpResponse 
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template import RequestContext, loader
 
+from dweet.models import Dweety
+from dweet.forms import DweetForm
 
 def dashboard(request):
     ctx = {}
 
+    ctx['dweetList'] = Dweety.objects.all()[:20]
+    ctx['dweetForm'] = DweetForm()
+
     ctx = RequestContext(request,ctx)
-    t = loader.get_template('base.html')
+    t = loader.get_template('dashboard.html')
+
+
+
 
     return HttpResponse(t.render(ctx))
+
+def dweetForm(request):
+    if request.method == 'POST':
+         form = DweetForm(request.POST)
+         if form.is_valid():
+             dweety = form.save(commit=False) #dont save the dweet, yet. We need to add user :)
+             dweety.user = request.user
+             dweety.save()
+
+    return HttpResponseRedirect('/')
