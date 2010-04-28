481989d6e0dd88e4d894e1057fb6cf9bb34de725
mozcelebi <mozcelebi@9b4810c6-2dba-11de-8e0c-53a27eea117e>
2010-04-28 12:04:08 +0000

    simple tag added

--- dweet/forms.py
+++ dweet/forms.py
@@ -5,4 +5,4 @@ class DweetForm(forms.ModelForm):
     id = forms.IntegerField(widget=forms.HiddenInput,required=False)
     class Meta:
         model = Dweety
-        fields = ('id','value')
+        fields = ('id','value', 'user')
